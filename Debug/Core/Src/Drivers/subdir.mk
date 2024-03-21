4de9aaad1da6dea1ca711223e8454478dcf8633d
Melta101 <70323154+malto101@users.noreply.github.com>
2024-03-21 17:44:59 +0530

    Initial Commit

---
+++ Debug/Core/Src/Drivers/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/Drivers/BMI088.c \
+../Core/Src/Drivers/SPL06.c 
+
+OBJS += \
+./Core/Src/Drivers/BMI088.o \
+./Core/Src/Drivers/SPL06.o 
+
+C_DEPS += \
+./Core/Src/Drivers/BMI088.d \
+./Core/Src/Drivers/SPL06.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/Drivers/%.o Core/Src/Drivers/%.su: ../Core/Src/Drivers/%.c Core/Src/Drivers/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F405xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src-2f-Drivers
+
+clean-Core-2f-Src-2f-Drivers:
+	-$(RM) ./Core/Src/Drivers/BMI088.d ./Core/Src/Drivers/BMI088.o ./Core/Src/Drivers/BMI088.su ./Core/Src/Drivers/SPL06.d ./Core/Src/Drivers/SPL06.o ./Core/Src/Drivers/SPL06.su
+
+.PHONY: clean-Core-2f-Src-2f-Drivers
+
---
+++ Debug/Core/Src/EKF.d
@@ -0,0 +1,2 @@
+Core/Src/EKF.o: ../Core/Src/EKF.c ../Core/Inc/EKF.h
+../Core/Inc/EKF.h:
